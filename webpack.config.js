694f668bcd790e0b6b386e84c9743504849d92e4
simple-ice <simplewu233@gmail.com>
2020-07-30 03:22:51 +0800

    webpack 生成 HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         filename: '[name].[contenthash].js',
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'simple_ice',
+        template: 'src/assets/index.html'
+    })]
 };
